b2d84383a730ef9c56f35efab8ee3b2054975467
redkrayn <redkrayn@yandex.ru>
2025-08-17 18:57:15 +0800

    Sort import

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from places.views import show_point, show_place
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+
+from places.views import show_point, show_place
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
